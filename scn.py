309fac260822d6d0f4b33f252bb8d8d721c0db2d
j0KL <94054254+j0KL@users.noreply.github.com>
2023-06-26 08:23:25 +0300

    Update scn.py

--- scn.py
+++ scn.py
@@ -63,16 +63,12 @@ def scan_open_ports(G, ip_address):
         G.add_edge(ip_address, neighbor)
 
 def create_network_map(ip_address):
-    # Сканирование сети и получение списка устройств
     devices = scan_network(ip_address)
 
-    # Создание графа сети
     G, labels = create_network_graph(devices)
-
-    # Добавление соединений между устройствами на граф
+    
     add_connections_to_graph(G, devices)
 
-    # Визуализация графа
     pos = nx.spring_layout(G)
     nx.draw_networkx_nodes(G, pos, node_size=500)
     nx.draw_networkx_edges(G, pos, alpha=0.3)
@@ -81,11 +77,6 @@ def create_network_map(ip_address):
 
 if __name__ == '__main__':
     start_time = time.time()
-    #Реализовать выбор/ввод/получение данных из файла изначального сканирования
-    # И производить построение карты по данным полученным из файла сканирования, а не сканировать заново 
-    # ip_address = '192.168.0.0/24' and int(input("Enter the ip and mask(example: 192.168.0.0/24): "))
-    # ip_address = input("Enter the ip and mask(example: 192.168.0.0/24): ")
-    ip_address = '192.168.0.0/24'
     create_network_map(ip_address)
     end_time = time.time()
     print(f"Время выполнения скрипта: {end_time - start_time} секунд")
